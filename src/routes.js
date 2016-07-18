e6d86fd1c3a6fd707f0eebdaeae23965cbe540cf
kcmartin <kcmartin@users.noreply.github.com>
2016-07-18 12:33:01 -0700

    Simple test of React Router

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts-index';
+
+// a little test component
+const Greeting = () => {
+  return <div>Hey there!</div>;
+};
+
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
+);
