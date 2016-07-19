d65ce50b9f73baba56dbbeb3963c719b0f3741b6
kcmartin <kcmartin@users.noreply.github.com>
2016-07-19 15:11:09 -0700

    Refactored posts index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
-};
+class PostsIndex extends Component {
+  // lifecycle method-only called once
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+// shortcut to avoid mapdispatchtoprops
+export default connect (null, { fetchPosts })(PostsIndex);
