c39bf5463fa005eed0ac1c233b3e6e40baa9f1f5
kcmartin <kcmartin@users.noreply.github.com>
2016-07-20 13:50:23 -0700

    Added basic form validation
    Tested on one field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,6 +12,9 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
@@ -31,11 +34,22 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  return errors;
+}
+
 // connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
 // redux-form: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 
 export default reduxForm({
   //configuration for reduxForm
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
